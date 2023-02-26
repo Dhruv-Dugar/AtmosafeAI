65ea54c1bb81215124531ea7539ef4ed140bacdf
Dhruv-Dugar <dhruvdugar02@gmail.com>
2023-02-27 01:01:55 +0530

    UI for the explaination view

--- PollutionPrediction/ExplainationView.swift
+++ PollutionPrediction/ExplainationView.swift
@@ -13,11 +13,95 @@ struct ExplainationView: View{
     
     var body: some View{
         VStack{
-            Text("Hi")
+//            Spacer()
+            HStack{
+                Spacer()
+                
+                Image(systemName: "ipad")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 75)
+                
+                Spacer()
+                
+                Image(systemName: "iphone")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 75)
+                
+                Spacer()
+                
+                Image(systemName: "ipad.homebutton")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 75)
+                
+                Spacer()
+                
+                Image(systemName: "iphone.homebutton")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 75)
+                
+                Spacer()
+            }
+            .padding(.top, 20)
+            .padding(.bottom, 30)
+
+            Text("PollutionPredictor has been optimised to work for all iPad and iPhone models.")
+                .titleStyle()
+                            
+            Spacer()
+            
+            (
+                Text("Pollution measurement is done in 6 key parameters, PM 2.5, PM 10, CO, NO_{2}, SO2 and O3.")
+                
+                +
+                
+                Text(" Out of these, ")
+                
+                +
+                
+                Text("PM 2.5 (fine particles)")
+                    .foregroundColor(.red)
+                
+                +
+                
+                Text(" are the most harmful, and being the most difficult to measure and control.")
+            )
+            .font(.system(size: 22))
+            .titleStyle()
+            
+            
+            Text("PollutionPredictor tries to predict the PM 2.5 values, and recommend if you should wear a mask while heading outside or not, based on the conditions!").font(.system(size: 22))
+                .titleStyle()
+            
+            Spacer()
+            
+            
+            
         }
+    }
+
+}
+
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .multilineTextAlignment(.center)
+            .font(.system(size: 30))
+            .padding()
+            .fixedSize(horizontal: false, vertical: true)
     }
 }
 
+
+extension View{
+    func titleStyle() -> some View{
+        modifier(Title())
+    }
+}
 
 
 struct ExplainationView_previews: PreviewProvider {

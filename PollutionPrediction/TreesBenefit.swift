f2da5335857854a509e0b9ba50d2086fd74cf610
Dhruv-Dugar <dhruvdugar02@gmail.com>
2023-04-07 21:31:00 +0530

    added navigation to next page

--- PollutionPrediction/TreesBenefit.swift
+++ PollutionPrediction/TreesBenefit.swift
@@ -7,56 +7,66 @@
 
 import Foundation
 import SwiftUI
-
+import SceneKit
 
 struct TreesBenefit: View{
     
     @State private var isShowing = false
+    @State private var isShowingRealityView = false
+    
+    
     
     var body: some View{
-        VStack {
-            
-            Spacer()
-            
-            Image("tree")
-                .resizable()
-                .foregroundColor(.green)
-                .aspectRatio(contentMode: .fit)
-            
-            
-            Spacer()
-            (
-                Text("Trees in cities reduce pollution by absorbing harmful gases, purifying the air and providing shade. Planting more trees in urban areas can improve air quality and create a healthier environment for people.")
-            )
-            .foregroundColor(.orange)
-            .multilineTextAlignment(.center)
-            
-            .lineLimit(5)
-            .padding()
-            .titleStyle()
-            
-            Spacer()
-            
-            Text("Green cities, clean air - plant a tree, show you care!")
-                .font(.system(size: 37, weight: .semibold, design: .monospaced))
-                .foregroundColor(.teal)
+        NavigationView{
+            VStack {
+                
+                Spacer()
+                
+                Image("tree")
+                    .resizable()
+                    .foregroundColor(.green)
+                    .aspectRatio(contentMode: .fit)
+                
+                
+                Spacer()
+                (
+                    Text("Trees in cities reduce pollution by absorbing harmful gases, purifying the air and providing shade. Planting more trees in urban areas can improve air quality and create a healthier environment for people.")
+                )
+                .foregroundColor(.orange)
+                .multilineTextAlignment(.center)
+                
+                .lineLimit(5)
+                .padding()
                 .titleStyle()
+                
+                Spacer()
+                
+                Text("Green cities, clean air - plant a tree, show you care!")
+                    .font(.system(size: 37, weight: .semibold, design: .monospaced))
+                    .foregroundColor(.teal)
+                    .titleStyle()
+                
+                Spacer()
+                
+                
+                NavigationLink(destination: TreeRealityView().toolbar(.hidden).navigationViewStyle(.stack), label: {
+                    Image(systemName: "arrow.right.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50)
+                        .foregroundColor(.blue)
+                })
+                
             
-            Spacer()
-            
-            Button{
-                // open TreeRealityView in a opened view
-            } label: {
-                Text("Click here and see the clean and green cities, for a better tomororw!")
             }
-        
-        }
-        .opacity(isShowing ? 1 : 0)
-        .onAppear{
-            withAnimation(.easeIn(duration: 1)) {
-                isShowing = true
+            .opacity(isShowing ? 1 : 0)
+            .onAppear{
+                withAnimation(.easeInOut(duration: 1)) {
+                    isShowing = true
+                }
             }
-        }
+        }.navigationBarHidden(true)
+            .navigationViewStyle(.stack)
         
     }
     
